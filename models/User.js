05999154c11df52a76fba4aa097be19449047b71
Hee Beom Park <skdnfl4343@gmail.com>
2021-11-30 20:54:37 +0900

    회원가입시 mystore 생성하도록 변경

--- models/User.js
+++ models/User.js
@@ -12,6 +12,8 @@ class User {
 
         var sql = `INSERT INTO ${table} SET ?`
         db.query(sql,memberInfo,callback)
+        var sql = `INSERT INTO mystore(store_name,store_description,store_image,member_id) VALUES('상점 이름','상점 설명','상점 사진',"${memberInfo.id}")`
+        db.query(sql,memberInfo)
     }
 }
 module.exports = User
