9e12a287c42ef3bd2c3e32530b5d96751820ebaa
Hee Beom Park <skdnfl4343@gmail.com>
2021-12-01 10:15:13 +0900

    Board 모델 아이디,게시물넘버쿼리문 추가

--- models/Board.js
+++ models/Board.js
@@ -9,8 +9,20 @@ class Board {
         var sql = `INSERT INTO ${table} SET ?`
         db.query(sql,boardsContent,callback)
     }
-    getBoardByNo =( callback)=>{
+    getBoard =( callback)=>{
         var sql = `SELECT * FROM ${table} `
+        db.query(sql,callback)
+    }
+    getBoardBySearch =( query,callback)=>{
+        var sql = `SELECT title,content FROM ${table} WHERE title LIKE ?`
+        db.query(sql,'%'+query+'%',callback)
+    }
+    getBoardById =(mystoreContent, callback)=>{
+        var sql = `SELECT * FROM ${table} where member_id = "${mystoreContent.member_id}"`
+        db.query(sql,callback)
+    }
+    getBoardByNo =(board_no, callback)=>{
+        var sql = `SELECT * FROM ${table} where board_no = "${board_no}"`
         db.query(sql,callback)
     }
 }
