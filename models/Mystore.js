093f4fcde80409e6c97d4c225cd5a894ebc7814e
Hee Beom Park <skdnfl4343@gmail.com>
2021-12-01 10:16:02 +0900

    mystore 모델 쿼리문 추가

--- models/Mystore.js
+++ models/Mystore.js
@@ -5,9 +5,14 @@ class Mystore {
 
     changeMystore =(mystoreContent, callback)=>{
         //console.log(member)
-
         var sql = `UPDATE ${table} SET ? where member_id = "${mystoreContent.member_id}"`
         db.query(sql,mystoreContent,callback)
     }
+    getMystoreById =(member_id, callback)=>{
+        //console.log(member)
+        var sql = `SELECT * FROM ${table} where member_id = "${member_id}"`
+        db.query(sql,callback)
+    }
+
 }
 module.exports = Mystore
