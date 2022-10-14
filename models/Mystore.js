e77c9f9031c0b0f5bfd5cad303242666b130a42b
qqOwOqq <skdnfl4343@gmail.com>
2022-10-14 21:42:13 +0900

    getId sql join문으로 변경

--- models/Mystore.js
+++ models/Mystore.js
@@ -8,7 +8,7 @@ class Mystore {
         db.query(sql,mystoreContent,callback)
     }
     getMystoreById =(member_id, callback)=>{
-        var sql = `SELECT * FROM ${table} join board where mystore.member_id = "${member_id}" and board.member_id = "${member_id}" order by date desc;`
+      var sql = `SELECT * FROM ${table} join board where mystore.member_id = "${member_id}" and board.member_id = "${member_id}" order by date desc;`
         db.query(sql,callback)
     }
 
