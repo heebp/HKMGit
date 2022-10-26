8f7b70c6fa72b19114c94475832187b606bda5f6
qqOwOqq <skdnfl4343@gmail.com>
2022-10-26 18:48:26 +0900

    메세지 db 저장,출력 컨트롤러

--- router/chatController/chat.js
+++ router/chatController/chat.js
@@ -4,13 +4,14 @@ const app = express()
 const session = require('express-session');
 const moment = require('moment')
 const ChatModel = require('../../models/Chat')
+const MessageModel = require('../../models/Message')
 const MySQLStore = require("express-mysql-session")(session);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-exports.chat = (req,res)=>{
+const chat = (req,res)=>{
   //console.log("req",req.params.board_no)
   var io = req.app.get('socketio');
   //var board_room = req.params.board_no
@@ -21,13 +22,18 @@ exports.chat = (req,res)=>{
     *   해결))) 시작 부분에 모든 리스너 삭제
     */
     socket.removeAllListeners()
-      console.log("chat")
       socket.on('chat message', function(data) {
           data.room = chat_no
           var name = socket.name = data.name;
           var room = socket.room = data.room;
-          console.log('message from client: ', data);
-
+          //console.log('message from client: ', data);
+          new MessageModel().createChatMessage(data,(error,results)=>{
+            if(error){
+              console.log("create message error : ", error)
+            }else{
+              console.log("create messgea results : ",results)
+            }
+          })
           // room에 join한다
           socket.join(room);
           // room에 join되어 있는 클라이언트에게 메시지를 전송한다
@@ -39,12 +45,16 @@ exports.chat = (req,res)=>{
     if(error){
       console.log("crete chat error : ",error)
     }else{
-      console.log("crete chat results : ",results)
-      return res.render("chatting",{
+      //console.log("crete chat results : ",results[1])
+      res.render("chatting",{
         chat_no : chat_no,
-        data : results
-          
+        data : results[0],
+        old_message : results[1]
       })
     }
   })
+}
+
+module.exports = {
+  chat
 }
