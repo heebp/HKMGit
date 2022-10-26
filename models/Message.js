2896fdbfbc74ac48cffa78453e7a7517d5f52ddc
qqOwOqq <skdnfl4343@gmail.com>
2022-10-26 18:47:16 +0900

    메세지 db 저장

--- models/Message.js
+++ models/Message.js
@@ -2,5 +2,13 @@ const db = require('../config/db')
 const table = 'message';
 
 class Message{
-
+  getChatMessage = (chat_no, callback) => {
+    var sql = `select chat_no,chat_id,message,chat.date,member_id,board_board_no from ${table} join chat on chat_chat_no = chat_no where chat_no =${chat_no};`;
+    db.query(sql,callback)
+  }
+  createChatMessage = (messageContent, callback) => {
+    var sql = `insert into ${table} values(${messageContent.room}, "${messageContent.id}", "${messageContent.message}","${messageContent.date}") `;
+    db.query(sql,callback)
+  }
 }
+module.exports = Message
